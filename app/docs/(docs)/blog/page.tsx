b920f796b49098401eb191ff993a65dc27ee4f0d
dripz0r <kieranreilly@Miriams-MacBook-Pro.local>
2025-06-15 03:51:52 -0400

    blogs working

---
+++ app/docs/(docs)/blog/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { DocsLayout } from "../../../components/layout/DocsLayout";
+import PageContent from "./index.mdx";
+
+export default function BlogDocsPage() {
+  return (
+    <DocsLayout>
+      <PageContent />
+    </DocsLayout>
+  );
+} 
